5aad64c02b031ae03a390664c824107af382b24a
Ong Zhixian <zhixian@hotmail.com>
2018-12-07 13:49:58 +0800

    update

--- models/test.py
+++ models/test.py
@@ -11,6 +11,10 @@ from helpers.app_helpers import *
 # from helpers.jinja2_helpers import *
 # from helpers.mongodb_helpers import *
 # from modules.scheduling import Roster
+from google.appengine.ext import ndb
+from Crypto.Hash import SHA256
+from datetime import datetime
+from datetime import timedelta
 
 ################################################################################
 # Setup helper functions
@@ -32,3 +36,110 @@ class TestModel:
     
     def GetId(self):
         return self.id
+
+class Account(ndb.Model):
+    username = ndb.StringProperty()
+    userid = ndb.IntegerProperty()
+    email = ndb.StringProperty()
+
+class Revision(ndb.Model):
+    message_text = ndb.StringProperty()
+
+
+class Login(ndb.Model):
+    login_name = ndb.StringProperty()
+    password = ndb.StringProperty()
+
+    @staticmethod
+    def register_login2(urlsafe_key, quantity):
+        blazer_size_key = ndb.Key(urlsafe=urlsafe_key)
+        blazer_size = blazer_size_key.get()
+        last_registered_number = blazer_size.last_registered_number
+
+########################################
+# Generic models
+########################################
+
+class Article(ndb.Model):
+    title = ndb.StringProperty()
+    stars = ndb.IntegerProperty()
+    tags = ndb.StringProperty(repeated=True)
+
+class Task(ndb.Model):
+    title = ndb.StringProperty()
+    stars = ndb.IntegerProperty()
+    tags = ndb.StringProperty(repeated=True)
+    
+class Group(db.Model):
+    name = ndb.StringProperty()
+    description = ndb.TextProperty()
+
+class Address(ndb.Model):
+    type = ndb.StringProperty()  # E.g., 'home', 'work', 'billing', 'lading'
+    line1 = ndb.StringProperty()
+    line2 = ndb.StringProperty()
+    line3 = ndb.StringProperty()
+    line4 = ndb.StringProperty()
+    number_of_lines = ndb.IntegerProperty()
+    # street = ndb.StringProperty()
+    # city = ndb.StringProperty()
+
+
+##### Example of polyModel
+class Contact(polymodel.PolyModel):
+    phone_number = ndb.PhoneNumberProperty()
+    address = ndb.PostalAddressProperty()
+
+class PersonContact(Contact):
+    first_name = ndb.StringProperty()
+    last_name = ndb.StringProperty()
+    mobile_number = ndb.PhoneNumberProperty()
+
+class CompanyContact(Contact):
+    name = ndb.StringProperty()
+    fax_number = ndb.PhoneNumberProperty()
+
+
+class UserProfile(ndb.Model):
+    access = ndb.KeyProperty(kind=Group, repeated=True)
+
+
+# class Contact(db.Model):
+#     # ID of user that owns this entry.
+#     owner = db.StringProperty()
+
+#     # Basic info.
+#     name = db.StringProperty()
+#     birth_day = db.DateProperty()
+
+#     # Address info.
+#     address = db.PostalAddressProperty()
+
+#     # Company info.
+#     company_title = db.StringProperty()
+#     company_name = db.StringProperty()
+#     company_description = db.StringProperty()
+#     company_address = db.PostalAddressProperty()
+
+#     # Group affiliation
+#     groups = db.ListProperty(db.Key)
+
+class Company(ndb.Model):
+    """A model representing a company."""
+    name = ndb.StringProperty()
+    description = ndb.TextProperty()
+    company_address = ndb.StringProperty()
+    preferred_address = ndb.KeyProperty(Address)
+    addresses = ndb.StructuredProperty(Address, repeated=True)
+    contacts = ndb.StructuredProperty(Contact, repeated=True)
+
+
+class Employee(ndb.Model):
+    """A model representing an employee."""
+    first_name = ndb.StringProperty(required=True)
+    last_name = ndb.StringProperty(required=True)
+    display_name = ndb.StringProperty(required=True)
+    national_id = ndb.StringProperty(required=True)
+    email = ndb.StringProperty(required=True)
+    company = ndb.KeyProperty(Company)
+    #contacts = ndb.StructuredProperty(Contact, repeated=True)
--- pages/test.py
+++ pages/test.py
@@ -7,6 +7,10 @@ from helpers.app_helpers import *
 from helpers.page_helpers import *
 from helpers.jinja2_helpers import *
 
+from google.appengine.ext import ndb
+# Import models
+from models.test import Login,Account,Revision,Company
+
 ################################################################################
 # Setup helper functions
 ################################################################################
@@ -22,12 +26,133 @@ def display_home_page(errorMessages=None):
     context = get_default_context(request)
     if request.method == 'POST':
         logging.info("In test - POST")
-    #logging.info("In test - GET")
+        
+        # new_ent = Login()
+        # new_ent.login_name = "zhixian"
+        # new_ent.password = "password"
+        # new_ent_key = new_ent.put()
+
+        new_ent = Company()
+        new_ent.name = "Brahman"
+        new_ent_key = new_ent.put()
+
+        if new_ent_key:
+            logging.info("Record added")
+        else:
+            logging.info("No new entity key")
     return jinja2_env.get_template('html/test/home-page.html').render(context)
 
-@route('/test/register', method=['POST','GET'])
+
+@route('/test/method2', method=['POST','GET'])
 def display_register_page(errorMessages=None):
     context = get_default_context(request)
-    if request.method == 'POST':
-        pass
-    return jinja2_env.get_template('html/test/register-page.html').render(context)
+    try:
+        if request.method == 'POST':
+            logging.info("Testing method 2")
+            #qry = Login.query().order()
+            
+            qry = Login.query(Login.login_name == "zhixian")
+            qry_lst = qry.fetch(1)
+            if qry_lst:
+                logging.info("Has result. count is {0}".format(len(qry_lst)))
+                if len(qry_lst) > 0:
+                    res_obj = qry_lst[0]
+                    # add another object with ancestry
+                    new_ent = Login()
+                    sandy_key = ndb.Key(Login, 'sandy@example.com')
+                    new_ent.login_name = "sandy"
+                    new_ent.password = "password"
+                    new_ent.put()
+                else:
+                    logging.info("qry_lst length is less than 0")
+            else:
+                logging.info("No result from query.")
+    except Exception as ex:
+        logging.error(ex)
+
+    return jinja2_env.get_template('html/test/home-page.html').render(context)
+
+
+@route('/test/method3', method=['POST','GET'])
+def display_method3(errorMessages=None):
+    context = get_default_context(request)
+    try:
+        if request.method == 'POST':
+            logging.info("Testing method 3")
+            #qry = Login.query().order()
+            
+            qry = Login.query(Login.login_name == "zhixian")
+            qry_lst = qry.fetch(1)
+            if qry_lst:
+                logging.info("Has result. count is {0}".format(len(qry_lst)))
+                if len(qry_lst) > 0:
+                    res_obj = qry_lst[0]
+                    # add another object with ancestry
+                    new_ent = Login()
+                    sharlyn_key = ndb.Key(Login, 'sharlyn@example.com', parent=res_obj.key)
+                    new_ent.login_name = "sharlyn"
+                    new_ent.password = "password"
+                    new_ent.put()
+                else:
+                    logging.info("qry_lst length is less than 0")
+            else:
+                logging.info("No result from query.")
+    except Exception as ex:
+        logging.error(ex)
+
+    return jinja2_env.get_template('html/test/home-page.html').render(context)
+
+@route('/test/method4', method=['POST','GET'])
+def display_method4(errorMessages=None):
+    context = get_default_context(request)
+    try:
+        if request.method == 'POST':
+            logging.info("Testing method 4")
+            # account = Account(username='Sandy', userid=1234, email='sandy@example.com', id='sandy@example.com')
+            # account.put()
+            # return account.key.id()  # returns 'sandy@example.com'
+
+            # account_key = ndb.Key(Account, 'sandy@example.com')
+            # #Ask Datastore to allocate an ID.
+            # new_id = ndb.Model.allocate_ids(size=1, parent=account_key)[0]
+            # # Datastore returns us an integer ID that we can use to create the message key
+            # message_key = ndb.Key('Message', new_id, parent=account_key)
+            # # Now we can put the message into Datastore
+            # initial_revision = Revision(message_text='Hello', id='1', parent=message_key)
+            # initial_revision.put()
+
+            ancestry = []
+
+            qry = Login.query(Login.login_name == "zhixian")
+            qry_lst = qry.fetch(1)
+            user1 = qry_lst[0]
+
+            ancestry.append(user1.key.kind())
+            ancestry.append(user1.key.id())
+            # qry = Login.query(Login.login_name == "annie")
+            # qry_lst = qry.fetch(1)
+            # user2 = qry_lst[0]
+
+            qry = Company.query(Company.name == "Brahman")
+            qry_lst = qry.fetch(1)
+            company1 = qry_lst[0]
+
+            logging.info("Key kind:[{0}], id:[{1}]".format(company1.key.kind(), company1.key.id()))
+            ancestry.append(company1.key.kind())
+            ancestry.append(company1.key.id())
+
+            logging.info(len(ancestry))
+
+            #new_key = ndb.Key("Login", user1.key.id(), "Company", company1.key.id())
+            new_key = ndb.Key(flat=ancestry)
+
+            new_ent = Login(parent=new_key)
+            new_ent.login_name = "mary"
+            new_ent.password = "password"
+            new_ent.put()
+            # END if request.method == 'POST':
+            
+    except Exception as ex:
+        logging.error(ex)
+
+    return jinja2_env.get_template('html/test/home-page.html').render(context)
